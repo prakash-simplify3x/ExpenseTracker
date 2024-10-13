0d1e4779e979f5de4f21ff1e5c2e246efe3f9387
Prakash Saw <prakash.s@simplify3x.com>
2024-10-13 23:22:01 +0530

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by User on 13/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
